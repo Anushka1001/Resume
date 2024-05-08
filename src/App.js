30f68b982e31b8aa45a5c8a065a89e34ae103ed4
Anushka Bajpai <67208558+Anushka1001@users.noreply.github.com>
2024-05-08 14:55:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Home from './components/Home';
 
 function App() {
   return (
-    <div className='App'>
       <div className="container-fluid">
-        <div className='row' >
-          <div className='col-lg-3 bgBlack displaySetting1'><SidePanel /></div>
-          <div className='col-lg-9 displaySetting2 pdright0 pdleft0'><Home /></div>
+        <div className='row width-fit-content'>
+          <div className='col-xxl-3 col-xl-3 col-lg-3 bgBlack displaySetting1'><SidePanel /></div>
+          <div className='col-xxl-9 col-xl-9 col-lg-9 displaySetting2 pdright0 pdleft0'><Home /></div>
         </div>
       </div>
-    </div>
   );
 }
 
